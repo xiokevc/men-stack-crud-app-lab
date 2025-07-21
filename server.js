81a7af2038053d5902a4757c4e1927eb306b01ff
xiokevc <xiokevc@gmail.com>
2025-07-21 17:03:48 -0400

    get index

--- server.js
+++ server.js
@@ -8,12 +8,27 @@ const methodOverride = require('method-override');
 const dotenv = require('dotenv');
 const Car = require('./models/Car');
 
+dotenv.config();
+
+// Middleware
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
+
 // =================== Get ===================
 
-app.get('/', async (req,res) => {
-    res.send('hello, friend!');
+// Index
+app.get('/cars', async (req, res) => {
+  const cars = await Car.find();
+  res.render('index', { cars });
 });
-
 
 // =================== Start ===================
 
