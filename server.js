5baea808636a787ab40c5f272dfa80bd09bc72ee
xiokevc <xiokevc@gmail.com>
2025-07-21 15:49:59 -0400

    const update

---
+++ server.js
@@ -0,0 +1,13 @@
+// =================== Const ===================
+
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const dotenv = require('dotenv');
+const Car = require('./models/Car');
+
+// =================== Const ===================
+
+
+
+// =================== Start ===================
